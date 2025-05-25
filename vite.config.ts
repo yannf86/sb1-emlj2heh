650ec99463380ded252a17dc4431650b063fdde2
yannf86 <89811840+yannf86@users.noreply.github.com>
2025-05-25 16:30:12 +0200

    Update codebase with external changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -13,7 +12,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/dev/' : '/',
+  base: '/', // 🔥 Corrigé ici
   server: {
     historyApiFallback: true, // Pour gérer les routes React
   }
